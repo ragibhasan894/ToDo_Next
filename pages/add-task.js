0a0171b0b15e6ff1b5bdc878141b6a589a1099ea
ragib_hasan13 <hasan.ragib@softbdltd.com>
2022-07-26 16:54:54 +0600

    adding items using axios

--- pages/add-task.js
+++ pages/add-task.js
@@ -1,28 +1,68 @@
 import Link from 'next/link'
 
 export default function AddTask() {
-  return (
-    <div className="container">
-        <div className="todo-list-heading row m-3 border-bottom border-primary">
-          <div className="col-sm-6 text-start text-primary">
-            <h3># Add Item to List</h3>
-          </div>
-          <div className="col-sm-6">
-            <Link href="/">
-              <button className="btn btn-success float-end">Back to List</button>
-            </Link>
-          </div>
+
+    const saveItem = (e) => {
+        e.preventDefault();
+        const data = {};
+        data.title = document.getElementById("task-title").value;
+        // data.to_be_completed = new Date();
+
+        // console.log(data);
+
+        const axios = require('axios');
+
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                "Access-Control-Allow-Origin": "*"
+            }
+        };
+
+        // axios({
+        //     method:'post',
+        //     url:'api/save-items',
+        //     baseURL: 'http://localhost:8000/',
+        //     // data: data
+        //    })
+        //     .then(res => {
+        //         console.log(res.data);
+        //     })
+        //     .catch(err => {
+        //         console.log('error in request', err);
+        //     });
+
+        axios.post('http://localhost:8000/api/save-items', data, {axiosConfig})
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log('error in request', err);
+            });
+    };
+      
+    return (
+        <div className="container">
+            <div className="todo-list-heading row m-3 border-bottom border-primary">
+              <div className="col-sm-6 text-start text-primary">
+                <h3># Add Item to List</h3>
+              </div>
+              <div className="col-sm-6">
+                <Link href="/">
+                  <button className="btn btn-success float-end">Back to List</button>
+                </Link>
+              </div>
+            </div>
+    
+            <div className="col-sm-4 mx-auto mt-5">
+                <form>
+                    <div className="row form-group">
+                        <input name="title" id="task-title" className="form-control mb-4" placeholder="Enter Task Name.." />
+    
+                        <button onClick={saveItem} className='btn btn-success'>Save</button>
+                    </div>
+                </form>
+            </div>        
         </div>
-
-        <div className="col-sm-4 mx-auto mt-5">
-            <form>
-                <div className="row form-group">
-                    <input name="title" className="form-control mb-4" placeholder="Enter Task Name.." />
-
-                    <button className='btn btn-success'>Save</button>
-                </div>
-            </form>
-        </div>        
-    </div>
-  )
-}
+      )
+    }
