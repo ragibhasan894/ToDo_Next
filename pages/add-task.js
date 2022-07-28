33c1ea41abe9de5d2f07aaf807da6b5d0f47a6bd
ragib_hasan13 <hasan.ragib@softbdltd.com>
2022-07-28 17:49:34 +0600

    updated add.js - added react-hook-form and yup validation

--- pages/add-task.js
+++ pages/add-task.js
@@ -1,46 +1,37 @@
 import Link from 'next/link'
+import { useForm } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from "yup"
+
+const schema = yup.object({
+  title: yup.string().required(),
+  description: yup.string().required(),
+}).required();
+
+const axios = require('axios');
 
 export default function AddTask() {
 
-    const saveItem = (e) => {
-        e.preventDefault();
-        const data = {};
-        data.title = document.getElementById("task-title").value;
-        data.description = document.getElementById("task-description").value;
-
-        // console.log(data);
-
-        const axios = require('axios');
-
-        let axiosConfig = {
-            headers: {
-                'Content-Type': 'application/json;charset=UTF-8',
-                "Access-Control-Allow-Origin": "*"
-            }
-        };
-
-        // axios({
-        //     method:'post',
-        //     url:'api/save-items',
-        //     baseURL: 'http://localhost:8000/',
-        //     // data: data
-        //    })
-        //     .then(res => {
-        //         console.log(res.data);
-        //     })
-        //     .catch(err => {
-        //         console.log('error in request', err);
-        //     });
-
-        axios.post('http://localhost:8000/api/save-items', data, {axiosConfig})
-            .then(res => {
-                console.log(res.data);
-                window.location = "/"
-            })
-            .catch(err => {
-                console.log('error in request', err);
-            });
+  const { register, handleSubmit, formState:{ errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const onSubmit = (data) => {
+    let axiosConfig = {
+      headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          "Access-Control-Allow-Origin": "*"
+      }
     };
+
+    axios.post('http://localhost:8000/api/save-items', data, {axiosConfig})
+        .then(res => {
+            console.log(res.data);
+            window.location = "/"
+        })
+        .catch(err => {
+            console.log('error in request', err);
+        });
+  };
       
     return (
         <div className="container">
@@ -56,12 +47,15 @@ export default function AddTask() {
             </div>
     
             <div className="col-sm-4 mx-auto mt-5">
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="row form-group">
-                        <input name="title" id="task-title" className="form-control mb-4" placeholder="Enter Task Name..." />
-    
-                        <textarea name="description" id="task-description" className="form-control mb-4" placeholder="Task Details..."></textarea>
-                        <button onClick={saveItem} className='btn btn-success'>Save</button>
+                        <input {...register("title")} id="task-title" className="form-control" placeholder="Enter Task Name..." />
+                        <span className="text-danger mb-2">{errors.title?.message}</span>
+                        
+                        <textarea {...register("description")} id="task-description" className="form-control" placeholder="Task Details..."></textarea>
+                        <span className="text-danger mb-3">{errors.description?.message}</span>
+                        
+                        <button className='btn btn-success'>Save</button>
                     </div>
                 </form>
             </div>        
